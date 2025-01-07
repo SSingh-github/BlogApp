3f27eb5ae085df222b69cb1f330991f37edeac5c
sukhpreet-singh <ss0218308@gmail.com>
2025-01-07 16:01:00 +0530

    dynamic routing illustration

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
+#this is a view function
 def hello():
     return 'Hello, world!'
+
+#dynamic routing
+@app.route('/<name>')
+def print_name(name):
+    return 'Hi,{}'.format(name)
 
 
 if __name__ == '__main__':
